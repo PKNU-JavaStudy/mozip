6e2c68224b599f4dda1ff0d3a1d8aa6c9bb52722
Legday <chlwodud0327@naver.com>
2024-06-03 11:56:47 +0900

    Feat: 비밀번호 암호화 구현완료
    1. BCrypt 의존성 추가
    2. AuthService 에서 비밀번호 암호화 후 DB 전송

--- src/main/java/com/mozip/service/AuthService.java
+++ src/main/java/com/mozip/service/AuthService.java
@@ -3,6 +3,7 @@ package com.mozip.service;
 import com.mozip.domain.member.JoinRepository;
 import com.mozip.dto.resp.JoinMemberDto;
 import lombok.RequiredArgsConstructor;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +14,8 @@ public class AuthService {
     private final JoinRepository joinRepository;
 
     public void joinUser(JoinMemberDto joinMemberDto) {
+        // 비밀번호 암호화
+        joinMemberDto.setPassword(BCrypt.hashpw(joinMemberDto.getPassword(), BCrypt.gensalt()));
         joinRepository.joinMember(joinMemberDto);
     }
 }
