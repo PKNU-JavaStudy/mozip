33f99d3ca0bd231a57a22bed53b26290e6498fa1
Legday <chlwodud0327@naver.com>
2024-06-05 16:57:17 +0900

    [#27]Feat: 프로젝트 참여 시 이메일 전송
    1. 멤버참여 버튼 클릭시 해당 멤버 이메일을 통해 이메일 전송 구현 완료
    
    resolved: #27

--- src/main/java/com/mozip/service/EmailService.java
+++ src/main/java/com/mozip/service/EmailService.java
@@ -1,7 +1,11 @@
 package com.mozip.service;
 
 import com.mozip.domain.member.AuthRepository;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,8 +15,27 @@ import org.springframework.transaction.annotation.Transactional;
 public class EmailService {
     private final AuthRepository authRepository;
 
-    public void emailSender(int memberId){
-        String memberEmail = authRepository.findById(memberId);
-        System.out.println(memberEmail);
+    private final JavaMailSender mailSender;
+
+    public void emailSender(int memberId) {
+        String memberEamil = authRepository.findById(memberId);
+        String title = "제목입니다.";
+        String content = "<a href='http://localhost:8082'>mo.zip</a>";
+
+        String from = "chlwodud199720327@gmail.com";
+        String to = memberEamil;
+
+        try {
+            MimeMessage mail = mailSender.createMimeMessage();
+            MimeMessageHelper mailHelper = new MimeMessageHelper(mail, true, "utf-8");
+            mailHelper.setFrom(from);
+            mailHelper.setTo(to);
+            mailHelper.setSubject(title);
+            mailHelper.setText(content, true);
+
+            mailSender.send(mail);
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
