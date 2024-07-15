22728b4a13601429d19486a35a7bc604edd3987f
king-dong-gun <160683545+king-dong-gun@users.noreply.github.com>
2024-07-15 17:00:31 +0900

    [#163] Fix: 북마크 필터 수정
    1. mypage.html 모집중, 모집완료 strong 태그 수정
    2. mypage.js에 북마크 필터시 표시되는 북마크 갯수 업데이트 함수 생성
    
    resolved: #163

--- src/main/resources/static/js/mypage.js
+++ src/main/resources/static/js/mypage.js
@@ -1,6 +1,7 @@
 // 필터링 함수
 function filterProjects(filterValue) {
     const projects = document.querySelectorAll('.pj-content.bookmark');
+    let visibleCount = 0; // 표시되는 프로젝트의 수
 
     projects.forEach(project => {
         const status = project.getAttribute('data-status');
@@ -8,38 +9,43 @@ function filterProjects(filterValue) {
         // 전체
         if (filterValue === 'all') {
             project.style.display = 'block';
+            visibleCount++;
         }
         // 모집완료
         else if (filterValue === '0') {
             if (status === '0') {
                 project.style.display = 'block';
+                visibleCount++;
             } else {
                 project.style.display = 'none';
             }
-            console.log("모집완료 성공");
         }
         // 모집중
         else if (filterValue === '1') {
             if (status === '1') {
                 project.style.display = 'block';
+                visibleCount++;
             } else {
                 project.style.display = 'none';
             }
-            console.log("모집중 성공");
         }
     });
 
-    // 빈 프로젝트 목록 처리
+    // 프로젝트 목록
     const visibleProjects = document.querySelectorAll('.pj-content.bookmark[style="display: block;"]');
     const notProjectMessage = document.querySelector('.notproject');
+
     if (visibleProjects.length === 0) {
         notProjectMessage.style.display = 'block';
     } else {
         notProjectMessage.style.display = 'none';
     }
+
+    // 북마크한 글의 갯수 표시
+    document.querySelector('.bookmark-count').textContent = visibleCount;
 }
 
-// 필터 선택 이벤트 핸들러
+// 필터 선택시 핸들러
 function handleBookmarkFilter(event) {
     let selectedValue = event.target.value;
     filterProjects(selectedValue);
